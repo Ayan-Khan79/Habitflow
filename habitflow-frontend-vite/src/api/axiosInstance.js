a1781ebc5206e4981582cdf5d06b3daf6673f6ec
Mohammad Ayan <ayankhan790521@gmail.com>
2025-10-22 14:22:20 +0530

    Env

--- habitflow-frontend-vite/src/api/axiosInstance.js
+++ habitflow-frontend-vite/src/api/axiosInstance.js
@@ -1,18 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL, // dynamic base URL
   withCredentials: true,
 });
-
-
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
 
 export default axiosInstance;
